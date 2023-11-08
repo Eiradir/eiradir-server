7d040533f9adaa8b1d883414903722d3c45ab092
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2023-11-08 13:29:20 +0100

    fix: Fix connection entity being reset too early causing character cleanup to fail

--- server/src/main/kotlin/net/eiradir/server/player/event/PlayerLeftEvent.kt
+++ server/src/main/kotlin/net/eiradir/server/player/event/PlayerLeftEvent.kt
@@ -1,6 +1,5 @@
 package net.eiradir.server.player.event
 
 import com.badlogic.ashley.core.Entity
-import net.eiradir.server.player.GameCharacter
 
 data class PlayerLeftEvent(val connection: Entity, val entity: Entity)
