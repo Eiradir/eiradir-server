e450c6d5274d73ed614033b7ec386a28b9df3a79
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2023-11-08 11:43:18 +0100

    feat: Inlude game host and port in login token response

--- server/src/main/kotlin/net/eiradir/server/session/SessionRoutes.kt
+++ server/src/main/kotlin/net/eiradir/server/session/SessionRoutes.kt
@@ -6,15 +6,19 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import net.eiradir.server.auth.JwtAccountAuthentication
 import net.eiradir.server.auth.KeycloakPrincipal
+import net.eiradir.server.config.ServerConfig
 import net.eiradir.server.http.KtorInitializer
 
-class SessionRoutes(private val sessionManager: ServerSessionManager) : KtorInitializer {
+class SessionRoutes(
+    private val sessionManager: ServerSessionManager,
+    private val serverConfig: ServerConfig
+) : KtorInitializer {
     override fun Routing.configureRoutes() {
         authenticate {
             post("/authorize") {
                 val principal = call.principal<KeycloakPrincipal>()!!
                 val session = sessionManager.createSession(JwtAccountAuthentication(principal)) as LoginTokenSession
-                call.respond(LoginTokenResponse(session.username, session.token))
+                call.respond(LoginTokenResponse(session.username, session.token, serverConfig.host, serverConfig.port))
             }
         }
     }
