a445c0424ec2aa20a7c6cf8f55e709795311e92f
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2023-11-08 13:22:18 +0100

    fix: Fix map sync not receiving events causing empty map in dev

--- server/src/main/kotlin/net/eiradir/server/map/sync/MapSyncServerPlugin.kt
+++ server/src/main/kotlin/net/eiradir/server/map/sync/MapSyncServerPlugin.kt
@@ -1,11 +1,13 @@
 package net.eiradir.server.map.sync
 
 import net.eiradir.server.plugin.EiradirPlugin
+import net.eiradir.server.plugin.EventBusSubscriber
 import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 class MapSyncServerPlugin : EiradirPlugin {
     override fun provide() = module {
-        singleOf(::MapSyncEvents)
+        singleOf(::MapSyncEvents) bind EventBusSubscriber::class
     }
 }
